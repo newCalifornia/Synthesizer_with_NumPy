687280e7388c4205b6b4bc483ba79679fd806192
newCalifornia <50933820+newCalifornia@users.noreply.github.com>
2026-05-02 09:00:26 -0700

    Update sound_example_2.py

--- sound_example_2.py
+++ sound_example_2.py
@@ -2,7 +2,7 @@
 ## The test to generate sound with NumPy math functions 
 ## and wrapped with some custom functions
 ## to generate short tracks.
-## Oleg Z. 20026
+## Oleg Z. 2026
 ## Requires:
 ##     NumPy - comes with default Python istallation
 ##     sounddevice - Python interface to the sound card
